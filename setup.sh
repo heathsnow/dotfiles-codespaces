948bbe75ca2c618c48dbc750037f2f8d24c01468
Heath Snow <hsnow@impinj.com>
2022-05-16 00:36:26 +0000

    no need to rm zip

--- setup.sh
+++ setup.sh
@@ -6,6 +6,5 @@ mkdir -p $HOME/.vim/pack/plugins/start/
 
 curl -o /tmp/NERD_tree.zip https://www.vim.org/scripts/download_script.php?src_id=23731
 unzip -q /tmp/NERD_tree.zip -d $HOME/.vim/pack/plugins/start/NERD_tree/
-rm /tmp/NERD_TREE.zip
 vim -u NONE -c "helptags $HOME/.vim/pack/plugins/start/NERD_tree/doc" -c -q
 
