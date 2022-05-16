4b297749a5ab252a4e7c05a8587b6e2db7a66954
Heath Snow <hsnow@impinj.com>
2022-05-16 00:32:17 +0000

    setup simplify

--- setup.sh
+++ setup.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
 
-RED="\033[0;31m"
-YELLOW="\033[0;33m"
-GREEN="\033[0;32m"
-OCHRE="\033[38;5;95m"
-BLUE="\033[0;34m"
-WHITE="\033[0;37m"
-RESET="\033[0m"
-
-logn()  { printf -- "--> %s " "$*"; }
-logk()  { echo -e "${GREEN}✔${RESET}"; }
-
 mkdir -p $HOME/.vim/pack/plugins/start/
 
-logn 'Installing NERD_tree vim plugin...'
+# vim nerd-tree
 
-  curl -o /tmp/NERD_tree.zip https://www.vim.org/scripts/download_script.php?src_id=23731
-  unzip -q /tmp/NERD_tree.zip -d $HOME/.vim/pack/plugins/start/NERD_tree/
-  rm /tmp/NERD_TREE.zip
-  vim -u NONE -c "helptags $HOME/.vim/pack/plugins/start/NERD_tree/doc" -c -q
+curl -o /tmp/NERD_tree.zip https://www.vim.org/scripts/download_script.php?src_id=23731
+unzip -q /tmp/NERD_tree.zip -d $HOME/.vim/pack/plugins/start/NERD_tree/
+rm /tmp/NERD_TREE.zip
+vim -u NONE -c "helptags $HOME/.vim/pack/plugins/start/NERD_tree/doc" -c -q
 
-logk
